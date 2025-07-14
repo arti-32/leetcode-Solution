d5d849598c52a70893a83afa281835a537f6b4bf
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-07-14 16:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (98.22%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -17,7 +17,7 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    public boolean helper(TreeNode root,long min,long max){
+    boolean helper(TreeNode root,long min,long max){
         if(root==null){
             return true;
         }
@@ -26,6 +26,6 @@ class Solution {
         }
         boolean left = helper(root.left,min,root.val);
         boolean right = helper(root.right,root.val,max);
-        return left&&right;
+        return left&right;
     }
 }
