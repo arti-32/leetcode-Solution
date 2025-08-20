e8e7322d5081b69271103b7c3b6513caff2f8628
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-08-20 15:36:18 +0530

    Time: 4 ms (49.19%) | Memory: 52.4 MB (16.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,51 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int ans = 0;
         int n = grid.length;
         int m = grid[0].length;
-        int[][] visit = new int[n][m];
-        int count=0;
+        boolean[][] visit = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visit[i][j]==0 && grid[i][j]=='1'){
-                    count++;
-                dfs(grid,visit,i,j);
+                if(!visit[i][j] && grid[i][j]=='1'){
+                    ans++;
+                    dfs(grid,visit,i,j);
                 }
             }
         }
-        return count;
+        return ans;
     }
-    private void dfs(char[][] grid,int[][] visit,int ro,int co){
-        visit[ro][co] = 1;
-        int n= visit.length;
-        int m = visit[0].length;
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(ro,co));
+    public void dfs(char[][] grid,boolean[][] visit,int x,int y){
+        visit[x][y] = true;
         int[] drow = {-1,0,1,0};
         int[] dcol = {0,1,0,-1};
-
-        while(!q.isEmpty()){
-            int i = q.peek().first;
-            int j = q.peek().second;
-            q.remove();
-
-
-            for(int p=0;p<4;p++){
-                int delrow = i+drow[p];
-                int delcol = j+dcol[p];
-                if(delrow>=0 && delrow<n && delcol>=0 && delcol<m && grid[delrow][delcol]=='1' && visit[delrow][delcol]==0){
-                    visit[delrow][delcol] = 1;
-                    q.add(new Pair(delrow,delcol));
-                }
+        for(int i=0;i<4;i++){
+            int delr = x+drow[i];
+            int delc = y+dcol[i];
+            if(delr>=0 && delr<grid.length && delc>=0 && delc<grid[0].length && !visit[delr][delc] && grid[delr][delc]=='1'){
+                dfs(grid,visit,delr,delc);
             }
         }
 
-    }
-}
-class Pair{
-    int first;
-    int second;
-    public Pair(int first,int second){
-        this.first = first;
-        this.second = second;
     }
 }
