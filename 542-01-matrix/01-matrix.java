9e98ad4007fc68b3dccb3fc657ae0f6d0e53fdc5
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-08-22 18:18:11 +0530

    Time: 17 ms (18.28%) | Memory: 47.7 MB (29.84%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,10 +1,13 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Queue<Pair> q = new LinkedList<>();
         int n = mat.length;
         int m = mat[0].length;
-        int[][] ans = new int[n][m];
+        int[] drow = {-1,0,1,0};
+        int[] dcol = {0,1,0,-1};
+        Queue<Pair> q = new LinkedList<>();
         boolean[][] visit = new boolean[n][m];
+
+        int[][] ans = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
@@ -13,24 +16,22 @@ class Solution {
                 }
             }
         }
-        int[] drow = {-1,0,1,0};
-        int[] dcol= {0,1,0,-1};
         while(!q.isEmpty()){
             int x = q.peek().x;
             int y = q.peek().y;
-            int val = q.peek().ind;
+            int ind = q.peek().ind;
             q.poll();
-            
+
             for(int i=0;i<4;i++){
                 int delr = x+drow[i];
                 int delc = y+dcol[i];
-                if(delr>=0 && delr<n && delc>=0 && delc<m && mat[delr][delc]==1 && !visit[delr][delc]){
-                    ans[delr][delc] = val+1;
-                    q.add(new Pair(delr,delc,val+1));
+                if(delr>=0 && delr<n && delc>=0 && delc<m && !visit[delr][delc] && mat[delr][delc] ==1){
+                    ans[delr][delc] = ind+1;
+                    q.add(new Pair(delr,delc,ind+1));
                     visit[delr][delc] = true;
                 }
-                
             }
+
         }
         return ans;
     }
