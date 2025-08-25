8b96cca8f8058d11443a10595bd26ca9c1edfe40
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-08-25 11:30:18 +0530

    Time: 7 ms (15.69%) | Memory: 52.3 MB (16.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,30 +1,46 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int ans = 0;
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] visit = new boolean[n][m];
+        Queue<Pair> q = new LinkedList<>();
+        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!visit[i][j] && grid[i][j]=='1'){
-                    ans++;
-                    dfs(grid,visit,i,j);
+                    count++;
+                    visit[i][j] = true;
+                    q.add(new Pair(i,j));
+                    bfs(grid,visit,q);
                 }
             }
         }
-        return ans;
+        return count;
     }
-    public void dfs(char[][] grid,boolean[][] visit,int x,int y){
-        visit[x][y] = true;
+    public void bfs(char[][] grid,boolean[][] visit,Queue<Pair> q){
         int[] drow = {-1,0,1,0};
         int[] dcol = {0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int delr = x+drow[i];
-            int delc = y+dcol[i];
-            if(delr>=0 && delr<grid.length && delc>=0 && delc<grid[0].length && !visit[delr][delc] && grid[delr][delc]=='1'){
-                dfs(grid,visit,delr,delc);
+        while(!q.isEmpty()){
+            int x = q.peek().x;
+            int y = q.peek().y;
+            q.poll();
+            for(int i=0;i<4;i++){
+                int delr = x+drow[i];
+                int delc = y+dcol[i];
+                if(delr>=0 && delr<grid.length && delc>=0 && delc<grid[0].length && !visit[delr][delc] && grid[delr][delc]=='1'){
+                    visit[delr][delc] = true;
+                    q.add(new Pair(delr,delc));
+                }
             }
-        }
 
+        }
+    }
+}
+class Pair{
+    int x;
+    int y;
+    public Pair(int x,int y){
+        this.x = x;
+        this.y = y;
     }
 }
