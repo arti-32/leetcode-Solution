ae0bfec6de1373d2cceb84446381ec6f7a3f7494
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-09-02 16:06:42 +0530

    Time: 2 ms (66.69%) | Memory: 42.6 MB (49.49%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,20 +1,20 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
+        int ans = 0;
         boolean[][] visit = new boolean[n][m];
-        Queue<Pair> q = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 2){
-                    visit[i][j] = true;
+                if(grid[i][j]==2){
                     q.add(new Pair(i,j,0));
+                    visit[i][j] = true;
                 }
             }
         }
-        int ans = 0;
-        int[] drow = {-1,0,1,0};
-        int[] dcol = {0,1,0,-1};
+        int[] dr = {-1,0,1,0};
+        int[] dc = {0,1,0,-1};
         while(!q.isEmpty()){
             int x = q.peek().x;
             int y = q.peek().y;
@@ -22,12 +22,12 @@ class Solution {
             ans = Math.max(ans,val);
             q.poll();
             for(int i=0;i<4;i++){
-                int delr = x+drow[i];
-                int delc = y+dcol[i];
+                int delr = x+dr[i];
+                int delc = y+dc[i];
                 if(delr>=0 && delr<n && delc>=0 && delc<m && !visit[delr][delc] && grid[delr][delc]==1){
-                    visit[delr][delc] = true;
                     grid[delr][delc] = 2;
                     q.add(new Pair(delr,delc,val+1));
+                    visit[delr][delc] = true;
                 }
             }
         }
