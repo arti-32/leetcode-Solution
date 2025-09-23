fdfa3c20b488dce0111d30e66317e63a6b4f0fdb
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-09-23 11:00:38 +0530

    Time: 3 ms (96.39%) | Memory: 53 MB (42.74%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,24 +3,23 @@ class Solution {
         int i=0;
         int ans =0;
         for(int j=0;j<nums.length;j++){
-            if(nums[j]==1){
-                ans = Math.max(ans,j-i+1);
-                continue;
-            }
+            // if(nums[j]==1){
+            //     ans = Math.max(ans,j-i+1);
+            //     continue;
+            // }
             if(nums[j]==0){
                 if(k>0){
                     k--;
-                    ans = Math.max(ans,j-i+1);
-                    continue;
                 }
-                if(k==0){
+                else if(k==0){
                     while(nums[i]==1){
                         i++;
                     }
                     i++;
-                    ans = Math.max(ans,j-i+1);
                 }
             }
+            ans = Math.max(ans,j-i+1);
+
         }
         return ans;
     }
