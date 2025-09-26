1d7b29cc206adc9a22ca84b9df75089462532b0d
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-09-26 13:37:51 +0530

    Time: 94 ms (77.01%) | Memory: 49.2 MB (69.86%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -6,13 +6,15 @@ class Solution {
         Arrays.fill(isPrime,true);
         isPrime[0] = false;
         isPrime[1] = false;
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(isPrime[i]){
-                ans++;
-                for(int j=i*2;j<n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     isPrime[j] = false;
                 }
             }
+        }
+        for(int i=0;i<n;i++){
+            if(isPrime[i]) ans++;
         }
         return ans;
     }
