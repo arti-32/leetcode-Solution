988e83ef438e4bd06122348f6063257452ce998f
arti-32 <167232986+arti-32@users.noreply.github.com>
2025-09-26 22:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (17.17%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans ={-1,-1};
-        ans[0] = bSearch(nums,target,true);
-        ans[1] = bSearch(nums,target,false);
+        int[] ans = {-1,-1};
+        ans[0] = left(nums,target);
+        ans[1] = right(nums,target);
         return ans;
     }
-    public int bSearch(int[] nums,int target,boolean isFirst){
-        int ans=-1;
-        int start=0;
+    public int left(int[] nums,int target){
+        int start =0;
         int end = nums.length-1;
+        int ans=-1;
         while(start<=end){
-            int mid= start+(end-start)/2;
+            int mid = start+(end-start)/2;
             if(nums[mid]==target){
-                ans=mid;
-                if(isFirst){
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }
+                ans= mid;
+                end = mid-1;
             }
-            else if(target>nums[mid]){
-                start= mid+1;
+            else if(target<nums[mid]){
+                end = mid-1;
             }
             else{
-                end=mid-1;
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    public int right(int[] nums,int target){
+        int start =0;
+        int end = nums.length-1;
+        int ans=-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target){
+                ans= mid;
+                start = mid+1;
+            }
+            else if(target<nums[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
             }
         }
         return ans;
